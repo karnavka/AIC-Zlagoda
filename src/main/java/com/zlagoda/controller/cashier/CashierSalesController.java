969e3c8e52da330b7307cfc3ba650a3c60cc982a
zmknnn <kotunonna@gmail.com>
2026-04-14 22:03:12 +0300

    interface sales for cashier

--- src/main/java/com/zlagoda/controller/cashier/CashierSalesController.java
+++ src/main/java/com/zlagoda/controller/cashier/CashierSalesController.java
@@ -1,4 +1,39 @@
 package com.zlagoda.controller.cashier;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class CashierSalesController {
+
+    @FXML
+    private TextField upcField;
+
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    private Label totalLabel;
+
+    @FXML
+    private Label discountSumLabel;
+
+    @FXML
+    private Label toPayLabel;
+
+    @FXML
+    private void onAddProduct() {
+        System.out.println("Додати товар: " + upcField.getText());
+    }
+
+    @FXML
+    private void onCancel() {
+        table.getItems().clear();
+    }
+
+    @FXML
+    private void onCreate() {
+        System.out.println("Чек створено");
+    }
 }
