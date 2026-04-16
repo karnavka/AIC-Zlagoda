d667ca44558ce2557c8d723c5c896fca14b89c9d
zmknnn <kotunonna@gmail.com>
2026-04-16 21:17:29 +0300

    check interface

--- src/main/java/com/zlagoda/controller/cashier/CashierChecksController.java
+++ src/main/java/com/zlagoda/controller/cashier/CashierChecksController.java
@@ -1,4 +1,23 @@
 package com.zlagoda.controller.cashier;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
 public class CashierChecksController {
+
+    @FXML
+    private TextField searchCheckNumberField;
+
+    @FXML
+    private Pane checkDetailsBox;
+
+
+
+    @FXML
+    private void handleCloseDetails() {
+        checkDetailsBox.setVisible(false);
+        checkDetailsBox.setManaged(false);
+        searchCheckNumberField.clear();
+    }
 }
